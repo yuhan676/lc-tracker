f81b37a264da3e0bc7f4afca8ef3ed1f5c0e3c18
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-02-27 22:36:34 +0000

    File updated at 27/02/2025, 22:36:34

--- 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.java
+++ 209-MinimumSizeSubarraySum/209-MinimumSizeSubarraySum.java
@@ -1,14 +1,30 @@
 class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        int sum = 0, res = Integer.MAX_VALUE, i = 0;
-        for (int j = 0; j < nums.length; j++){
-            sum += nums[j];
-            while (sum >= target){
-                res = Math.min(res, j - i + 1);
-                sum -= nums[i];
-                i++;
+    public int[][] generateMatrix(int n) {
+        int nums [] [] = new int [n] [n];
+        int startX =0, startY = 0, loop = 1;
+        int i,j;
+        int count = 1, offset = 1;
+        while (loop <= n/2) {
+            for (j = startY;j < n-offset;j++){
+                nums[startX][j] = count++;
             }
+            for (i=startX; i < n-offset; i++){
+                nums[i][j] = count++;
+            }
+            for (; j > startY; j--){
+                nums[i][j] = count++;
+            }
+            for (; i> startX; i--){
+                nums[i][j] = count ++;
+            }
+            startX ++;
+            startY ++;
+            loop ++;
+            offset ++;
         }
-        return (res == Integer.MAX_VALUE) ? 0:res; 
+        if (n%2 == 1){
+            nums[startX][startY] = count;
+        }
+        return nums;
     }
 }
