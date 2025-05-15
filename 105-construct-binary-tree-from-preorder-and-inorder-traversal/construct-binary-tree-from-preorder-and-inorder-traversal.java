191fb3050c74bbb37942110888d99ae5fdd75638
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-05-15 16:11:17 +0100

    Time: 2 ms (61.59%) | Memory: 45.1 MB (13.03%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,27 +14,28 @@
  * }
  */
 class Solution {
+
+    Map<Integer,Integer> inorderMap;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTreeWithIndex(preorder,inorder);
+        inorderMap = new HashMap<>();
+        //记录inordermap为了快速查询rootvalue在inorder数组中的位置（左中右的中）
+        for (int i =0; i<inorder.length;i++){
+            inorderMap.put(inorder[i],i);
+        }
+
+        return buildTreeWithIndex(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
     }
 
-    private TreeNode buildTreeWithIndex(int[] preorder, int[] inorder){
-        if (preorder.length==0) return null;
-        TreeNode root = new TreeNode(preorder[0]);
-        if (preorder.length==1) return root;
-        int index = 0;
-        for (; index<inorder.length;index++){
-            if (inorder[index]==root.val){
-                break;
-            }
-        }
-        int[] leftinorder = Arrays.copyOfRange(inorder,0,index);
-        int[] rightinorder = Arrays.copyOfRange(inorder,index+1,inorder.length);
-        int[] leftpreorder = Arrays.copyOfRange(preorder,1,index+1);
-        int[] rightpreorder = Arrays.copyOfRange(preorder,index+1,preorder.length);
+    private TreeNode buildTreeWithIndex(int[] preorder, int preS, int preE, int[] inorder, int inS, int inE){
+        if (preS > preE || inS > inE) return null;
+        int rootval = preorder[preS];
+        TreeNode root = new TreeNode(rootval);
+        int index = inorderMap.get(rootval);
+        //得到左子树数组的大小
+        int leftSize = index - inS;
+        root.left = buildTreeWithIndex(preorder, preS+1, preS + leftSize, inorder, inS, index-1);
+        root.right = buildTreeWithIndex(preorder, preS + leftSize + 1, preE, inorder, index + 1, inE);
 
-        root.left = buildTreeWithIndex(leftpreorder,leftinorder);
-        root.right = buildTreeWithIndex(rightpreorder,rightinorder);
         return root;
     }
 }
