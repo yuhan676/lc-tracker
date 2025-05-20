94de0bb6585eb2684ee78516fc911fd2c76d2609
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-05-20 16:25:55 +0100

    Time: 2 ms (61.47%) | Memory: 44.3 MB (76.83%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,28 +14,24 @@
  * }
  */
 class Solution {
-
-    Map<Integer,Integer> inorderMap;
+    Map<Integer,Integer> inorderMap = new HashMap<>();
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        inorderMap = new HashMap<>();
-        //记录inordermap为了快速查询rootvalue在inorder数组中的位置（左中右的中）
+        if (preorder.length==0) return null;
         for (int i =0; i<inorder.length;i++){
             inorderMap.put(inorder[i],i);
         }
+        return helper(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
 
-        return buildTreeWithIndex(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
     }
 
-    private TreeNode buildTreeWithIndex(int[] preorder, int preS, int preE, int[] inorder, int inS, int inE){
-        if (preS > preE || inS > inE) return null;
-        int rootval = preorder[preS];
-        TreeNode root = new TreeNode(rootval);
-        int index = inorderMap.get(rootval);
-        //得到左子树数组的大小
-        int leftSize = index - inS;
-        root.left = buildTreeWithIndex(preorder, preS+1, preS + leftSize, inorder, inS, index-1);
-        root.right = buildTreeWithIndex(preorder, preS + leftSize + 1, preE, inorder, index + 1, inE);
-
+    private TreeNode helper(int[] preorder, int preS, int preE, int[] inorder, int inS, int inE){
+        if (preS > preE || inS >inE) return null;
+        int rootVal = preorder[preS];
+        int inorderRootIndex = inorderMap.get(rootVal);
+        int leftSize = inorderRootIndex - inS;
+        TreeNode root = new TreeNode(rootVal);
+        root.left = helper(preorder, preS + 1, preS + leftSize, inorder, inS, inorderRootIndex-1);
+        root.right = helper(preorder,preS + 1 + leftSize, preE, inorder, inS + 1 + leftSize, inE);
         return root;
     }
 }
