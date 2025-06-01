f0eee66dacaf5be3fc8f716378dc1b5b0dd99254
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-01 12:34:45 +0100

    Time: 32 ms (35.68%) | Memory: 55.3 MB (91.88%) - LeetSync

--- 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.java
+++ 2449-maximum-number-of-robots-within-budget/maximum-number-of-robots-within-budget.java
@@ -1,53 +1,59 @@
 class Solution {
     public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
-        //一共有n个robot可供选择
         int n = chargeTimes.length;
-        //滑动窗口+treemap
-        //storing chargeTimes, frequency
-        //因为我们要维护一个窗口内的最大chargetime
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        long rcsum = 0;
-        int left = 0, maxRobots = 0;
+        MonoDeque mq = new MonoDeque();
+        int left = 0;
+        long rcSum = 0;
+        long totalCost = 0;
+        int maxRobot = 0;
         for (int right = 0; right <n; right ++){
-            //get the new Element's chargetime & running cost
             int ct = chargeTimes[right];
             int rc = runningCosts[right];
-
-            //add it to the sliding window
-            //frequency ++
-            map.put(ct, map.getOrDefault(ct,0) + 1);
-            //rcsum also increment
-            rcsum += rc;
-            int windowSize = right - left + 1;
-            //treemap 特有的lastkey()得到最大的key
-            //O(logn)
-            long totalCost = map.lastKey() + windowSize * rcsum;
-            //如果超了budget就要从左边缩小window
+            mq.add(ct);
+            rcSum += rc;
+            int robotCount = right - left + 1;
+            //totalCost = (mq.peek() != null ? mq.peek():0) + robotCount * rcSum;
+            //这么写的话左边是int，右边是integer，有时候编译器无法拆箱,所以逻辑交给monodeque class
+            totalCost = mq.peek()+ (long)robotCount * rcSum;
+            //从左边界开始滑动删除元素
             while (totalCost > budget){
-                int leftCt = chargeTimes[left];
-                int leftRc = runningCosts[left];
-
-                //从窗口中删除left
-                //之前已经放进去过，就不要get or default 了
-                //frequency --
-                map.put(leftCt, map.get(leftCt)-1);
-                //清零了就删除
-                if (map.get(leftCt)==0){
-                    map.remove(leftCt);
-                }
-                rcsum -= leftRc;
+                int leftct = chargeTimes[left];
+                int leftrc = runningCosts[left];
+                mq.poll(leftct);
+                rcSum -= leftrc;
+                //别忘了left++
                 left ++;
-
-                windowSize = right - left + 1;
-                if (!map.isEmpty()){
-                   totalCost = map.lastKey() + windowSize * rcsum;
-                }else{
-                    totalCost = 0;
-                }
-                
+                robotCount = right - left + 1;
+                totalCost = mq.peek()+ (long)robotCount * rcSum;
             }
-            maxRobots = Math.max(maxRobots, windowSize);
+            maxRobot = Math.max(maxRobot, robotCount);
         }
-        return maxRobots;
+        return maxRobot;
+    }
+}
+//自定义一个单调队列，可以O(1)维护窗口内的最大值
+class MonoDeque{
+    Deque<Integer> deque = new LinkedList<>();
+
+    void poll(int val){
+        if (!deque.isEmpty() && deque.peek() == val){
+            deque.poll();
+        }
+    }
+
+    void add(int val){
+        while(!deque.isEmpty() && val>deque.getLast()){
+            deque.removeLast();
+        }
+        //加入到队尾
+        deque.add(val);
+    }
+
+    int peek(){
+        //现在假如为空会返回null，如果加入了检查是否为empty，是empty就返回0， 就可以规避这一点
+        if (!deque.isEmpty()){
+            return deque.peek();
+        }
+        return 0;
     }
 }
