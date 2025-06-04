e7636fd967bf77a6277d001e4fdca28174ca9755
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-04 19:15:12 +0100

    Time: 0 ms (100.00%) | Memory: 42.2 MB (95.47%) - LeetSync

--- 301-remove-invalid-parentheses/remove-invalid-parentheses.java
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.java
@@ -1,68 +1,65 @@
 class Solution {
     public List<String> removeInvalidParentheses(String s) {
-        Stack<Character> st = new Stack<>();
-        int removeRight = 0;
+        int leftRem = 0;
+        int rightRem = 0;
         for (char c: s.toCharArray()){
             if (c=='('){
-                st.push('(');
-            }else if(c==')'){
-                if (!st.isEmpty()){st.pop();}
-                else{
-                    removeRight ++;
+                leftRem ++;
+            }else if (c==')'){
+                if (leftRem>0){
+                    leftRem --;
+                }else{
+                    rightRem ++;
                 }
             }
         }
-        int removeLeft = st.size();
-        StringBuilder path = new StringBuilder();
-        Set<String> resSet = new HashSet<String>();
-        backtrack(s, 0, 0, removeLeft, removeRight, resSet, path);
-        return new ArrayList<>(resSet);
+        int validLength = s.length() - leftRem -rightRem;
+        char[] path = new char[validLength];
+        List<String> res = new ArrayList<>();
+        backtrack(s,0,path,0,leftRem,rightRem,0,res);
+        return res;
+
     }
-    //openleft用来监督当前stringbuilder里有没有开口的左括号，只有有的情况下，才能加上右边的括号才合法
-    //结果先用一个string set收集着方便去重
-    private void backtrack(String s, int index, int openLeft, int removeLeft, int removeRight, Set<String> res, StringBuilder path){
-        //终止条件:遍历到s最后一位了
-        if (index ==s.length()){
-            //只有当openLeft, removeLeft, removeRight 都为0的时候，结果才可以加入res
-            if (openLeft == 0 && removeRight == 0 && removeLeft == 0){
-                res.add(path.toString());
+    private void backtrack(String s, int index, char[] path, int pathIndex, int leftRem, int rightRem, int balance, List<String> res){
+        //balance: (.count - ).count, 如果多出来一个‘）’，那接下来的也就都剪枝了
+        if (balance < 0){
+            return;
+        }
+        if (index == s.length()){
+            if (leftRem == 0 && rightRem == 0){
+                res.add(new String(path));
             }
             return;
         }
         char c = s.charAt(index);
-        //记录当前stringbuilder长度方便回溯
-        int len = path.length();
-        //每个字节可以选择保留和不保留
-        if (c=='('){
-            //删除，就不需要修改path（path中不加入c），所以不需要回溯；
-            if (removeLeft>0){
-                backtrack(s, index+1, openLeft,removeLeft -1, removeRight, res, path);
-            }
-            //保留这个c，就需要修改path
-            path.append(c);
-            backtrack(s,index+1,openLeft+1,removeLeft,removeRight,res,path);
-            //递归完回溯：即删除刚才append的c
-            path.setLength(len);
-        }else if (c==')'){
-            //删除
-            if (removeRight>0){
-                //path不改变，不需要回溯
-                backtrack(s,index+1,openLeft,removeLeft,removeRight -1, res, path);
-            }
-            //只有前面还有openleft的时候，才能保留当前的右括号
-            if (openLeft>0){
-                path.append(c);
-                //递归
-                backtrack(s,index+1,openLeft-1,removeLeft,removeRight,res,path);
-                //回溯
-                path.setLength(len);
-            }
-        }else{
-            //普通字符，一定保留，即加入path
-            path.append(c);
-            backtrack(s,index+1,openLeft,removeLeft,removeRight,res,path);
-            //加入了就需要回溯
-            path.setLength(len);
+        //三种情况都写成if，就可以保证删或者不删的情况都能覆盖到
+        //如果写成if else...elseif..else,每次只执行一个分支(删除/保留），就无法同时探索“删除”和“保留”的两种路径，导致漏解。
+        //OP1
+        if (c == '(' && leftRem>0 && (pathIndex == 0 || path[pathIndex-1] != '(')){
+            //删除可以删除的(
+            //剪枝：想避免的是构建路径中的重复删除方式，所以检查的不是index==0 || s.charAt(index) == s.charAt(index-1)
+            //（（（：我们只删第一个 (，其他重复的 ( 只保留，不再尝试删除，防止生成重复的结果字符串。
+
+                backtrack(s,index+1,path,pathIndex,leftRem-1,rightRem,balance,res);
+            
         }
+        //OP2
+        if (c==')' && rightRem >0 && (pathIndex == 0 || path[pathIndex-1] != ')')){
+            //删除可以删除的)
+                backtrack(s,index+1,path,pathIndex,leftRem,rightRem-1,balance,res);
+        }
+        //OP3: 保留
+        if(pathIndex < path.length){
+            //保留当前character （可能是非括号字符，要么就是不需要删除左右括号的情况下）,加入path
+            path[pathIndex] = c;
+            int newBalance = balance;
+            if (c=='('){
+                newBalance ++;
+            }else if (c==')'){
+                newBalance --;
+            }
+            backtrack(s,index+1,path,pathIndex+1,leftRem,rightRem,newBalance,res);
+        }
+        
     }
 }
