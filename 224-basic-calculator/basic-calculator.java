76cdfd3868f2b8c0e702398558a76ef5fce7d2ca
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-04 12:53:01 +0100

    Time: 8 ms (73.58%) | Memory: 43.5 MB (83.82%) - LeetSync

--- 224-basic-calculator/basic-calculator.java
+++ 224-basic-calculator/basic-calculator.java
@@ -1,36 +1,43 @@
 class Solution {
     public int calculate(String s) {
-        s = s.replaceAll("\\s+","");
-        int n = s.length();
-
+        // sign before the (), previous calculation result
+        Stack<Integer> st = new Stack<>();
+        //num = number in construction
+        //result = 中程计算结果
         int num = 0;
         int res = 0;
-        //1 = +, -1 = -
+        int n = s.length();
+        //记录当前number的sign
         int sign = 1;
-        //"- (3 + (4 + 5))"的情况，不能一遇')'到就把flip消除掉
-        Stack<Integer> signStack = new Stack<>(); // -1 = -, 1 = +
-        signStack.push(1);
-        for (int i=0;i<n;i++){
+        for (int i=0; i<n;i++){
             char c = s.charAt(i);
             if (Character.isDigit(c)){
-                num = num*10 + c-'0';
-            }
-            else if (c=='+' || c =='-'){
-                res += signStack.peek() * sign * num;
+                num = num*10 + c - '0';
+            }else if (c=='+' || c=='-'){
+                res += sign * num;
                 num = 0;
-                if (c =='+'){sign = 1;}
-                else{sign = -1;}
-            }
-            else if(c=='('){
-                signStack.push(sign*signStack.peek());
+                sign = (c == '+') ? 1:-1;
+            }else if (c=='('){
+                //（）前结果存入栈
+                st.push(res);
+                //（）前sign存入栈
+                st.push(sign);
+                //reset
                 sign = 1;
+                //result现在只记录括号中的结果，所以要清零
+                res = 0;
             }else if (c==')'){
-                res += sign * signStack.pop() * num;
-                sign = 1;
+                //终止计算括号里所有数字的和
+                res += sign * num;
                 num = 0;
+                //给括号里的result加上外层包的sign
+                res *= st.pop();
+                //给res加上括号外面的运算结果
+                res += st.pop();
             }
         }
-        res += sign * signStack.peek() * num; 
+        //假设input仅仅是数字，没有任何符号，那res就还没有改变
+        if (num!= 0) {res += sign * num;}
         return res;
-    } 
+    }
 }
