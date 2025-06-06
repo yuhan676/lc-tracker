53970736b707c8d18f3b1e94bb850212e33f9192
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-06 17:35:03 +0100

    Time: 0 ms (100.00%) | Memory: 40.4 MB (65.76%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] ways = new int[2];
-        int temp;
-        //站在第0格只有一种方式：不走
-        ways[0] = 1;
-        //站在第1格只有一种方式：从0爬一格
-        ways[1] = 1;
-        for (int i =2; i<=n; i++){
-            temp = ways[0] + ways[1];
-            ways[0] = ways[1];
-            ways[1] = temp;
+        return climbStairsM(n,2);
+    }
+    private int climbStairsM(int n, int m){
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for (int i =1; i<=n;i++){
+            for (int j = 1; j<= m && i>= j;j++){
+                //最大情况下 i == j, i-j = 0,走到最后一步了
+                    dp[i] += dp[i-j];
+            }
         }
-        return ways[1];
-
+        return dp[n];
     }
 }
