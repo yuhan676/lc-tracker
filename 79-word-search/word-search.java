ef24d2de04a980d1778754a06b6b1175af2c342c
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-06 11:36:42 +0100

    Time: 166 ms (36.86%) | Memory: 41.7 MB (63.31%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,38 +1,37 @@
 class Solution {
 
-    final int[][] dir= {{1,0}, {0,1},{-1,0},{0,-1}};
-
+    final int[][] dir = {{1,0}, {0,1},{-1,0},{0,-1}};
     public boolean exist(char[][] board, String word) {
         boolean[][] visited = new boolean[board.length][board[0].length];
-        for (int i=0;i <board.length;i++){
+        char[] path = new char[word.length()];
+        for (int i=0; i<board.length;i++){
             for (int j=0; j<board[0].length;j++){
                 if (board[i][j] == word.charAt(0)){
-                    if (dfs(board,visited,word,0,i,j)) return true;
+                    if (dfs(board,word,path,visited,i,j,0)) return true;
                 }
             }
         }
         return false;
     }
 
-    private boolean dfs(char[][] board, boolean[][] visited, String word, int index, int x, int y){
-        if (index == word.length()) return true;
-        //判断直接在这里判断
-        //visited，越界，是否当前wordindex仍然合法
-        if (x < 0 || y <0 || x >= board.length || y >= board[0].length || word.charAt(index) != board[x][y]|| visited[x][y]) return false;
-
+    private boolean dfs(char[][] board, String word,char[] path, boolean[][] visited, int x, int y, int index){
+        if (word.charAt(index) != board[x][y]){
+            return false;
+        }
+        if (index == word.length()-1){
+            return true;
+        }
+        path[index] = board[x][y];
         visited[x][y] = true;
-
-        for (int i =0; i<4;i++){
-            int newX = x + dir[i][0];
-            int newY = y + dir[i][1];
-            //这里要传递下一层的boolean值回去
-            if (dfs(board,visited,word,index+1,newX,newY)){
-                return true;
+        for(int[] d: dir){
+            int newX = x + d[0];
+            int newY = y + d[1];
+            if (newX < 0 || newY <0 || newX >= board.length || newY >=board[0].length || visited[newX][newY]){
+                continue;
             }
+            if (dfs(board,word,path,visited,newX,newY,index+1)) return true;
         }
         visited[x][y] = false;
-        //所有方向试过都走不通才return false
         return false;
-
     }
 }
