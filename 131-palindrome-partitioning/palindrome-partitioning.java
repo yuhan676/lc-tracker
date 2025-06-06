2012de59f003b129f686508b505e6de304b8635f
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-06 16:10:46 +0100

    Time: 10 ms (19.61%) | Memory: 64.7 MB (5.41%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,67 +1,32 @@
-// class Solution {
-//     List<List<String>> res = new LinkedList<>();
-//     List<String> path = new LinkedList<>();
-
-//     public List<List<String>> partition(String s) {
-//         StringBuilder sb = new StringBuilder();
-//         backtrack(s,sb,0);
-//         return res;
-
-//     }
-//     private void backtrack(String s, StringBuilder sb, int startIndex){
-//         if (startIndex == s.length()){
-//             res.add(new LinkedList<>(path));
-//             return;
-//         }
-//         for (int i = startIndex; i <s.length();i++){
-//             sb.append(s.charAt(i));
-//             if (isPalindrome(sb)){
-//                 //是的话，path里就要加上了
-//                 path.add(sb.toString());
-//                 backtrack(s,new StringBuilder(),i+1);
-//                 path.remove(path.size()-1);
-//             }
-//             sb.deleteCharAt(sb.length()-1);
-//         }
-        
-//     }
-
-//     private boolean isPalindrome(StringBuilder sb){
-//         for (int i = 0; i< sb.length()/2;i++){
-//             if (sb.charAt(i) != sb.charAt(sb.length()-1-i)) return false;
-//         }
-//         return true;
-//     }
-// }
 class Solution {
-  List<List<String>> res = new ArrayList<>();
-  List<String> path = new ArrayList<>();
-  public List<List<String>> partition(String s) {
-      backtracking(s, 0, new StringBuilder());
-      return res;
-  }
+    List<List<String>> res = new LinkedList<>();
+    List<String> path = new LinkedList<>();
 
-  private void backtracking(String s, int startIndex, StringBuilder sb){
-      if (startIndex == s.length()){
-          res.add(new ArrayList<>(path));
-      }
-      for (int i = startIndex; i<s.length();i++){
-          sb.append(s.charAt(i));
-          if (isPalindrome(sb)){
-              path.add(sb.toString());
-              //遇到了合适的结果才递归
-              backtracking(s,i+1,new StringBuilder());
-              //这里撤销意味着这条path的内容已经满了，回溯把path撤回空的状态，好找下一种切割方式切出来path的结果
-              path.remove(path.size()-1);
-          }
-      }
+    public List<List<String>> partition(String s) {
+        backtrack(s,0);
+        return res;
 
-  }
+    }
+    private void backtrack(String s, int startIndex){
+        if (startIndex == s.length()){
+            res.add(new LinkedList<>(path));
+            return;
+        }
+        for (int i = startIndex; i <s.length();i++){
+            String sbs = s.substring(startIndex, i+1);
+            if (isPalindrome(sbs)){
+                path.add(sbs);
+                backtrack(s,i+1);
+                path.remove(path.size()-1);
+            }
+        }
+        
+    }
 
-  private boolean isPalindrome(StringBuilder sb){
-      for(int i = 0; i<sb.length()/ 2; i++){
-          if (sb.charAt(i)!= sb.charAt(sb.length()-1-i)){return false;}
-      }
-      return true;
-  }
+    private boolean isPalindrome(String s){
+        for (int i = 0; i< s.length()/2;i++){
+            if (s.charAt(i) != s.charAt(s.length()-1-i)) return false;
+        }
+        return true;
+    }
 }
