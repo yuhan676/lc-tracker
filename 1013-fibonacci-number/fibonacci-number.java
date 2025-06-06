dd66b5015188b99ce12f97087dcda751cbb540a1
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-06 16:54:43 +0100

    Time: 0 ms (100.00%) | Memory: 40.7 MB (28.51%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public int fib(int n) {
         if (n == 0 || n == 1) return n;
-        int[] fib = new int[n+1];
+        int[] fib = new int[2];
         fib[0] = 0;
         fib[1] = 1;
         for(int i =2; i<=n;i++){
-            fib[i] = fib[i-1] + fib[i-2];
+            int temp = fib[0] + fib[1];
+            fib[0] = fib[1];
+            fib[1] = temp;
         }
-        return fib[n];
+        return fib[1];
     }
 }
