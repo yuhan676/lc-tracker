6d32b95b8912ea27ab19b9734cdbe1ede831b176
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-08 12:24:05 +0100

    Time: 0 ms (100.00%) | Memory: 40.6 MB (26.89%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,24 +1,17 @@
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
-
     public int numTrees(int n) {
-        return getBstNum(n);
-    }
-
-    private int getBstNum(int n){
-        if (n == 0 || n ==1) return 1;
-        if (memo.containsKey(n)) return memo.get(n);
-        int res = 0;
-        //遍历所有头节点可能性
-        for (int i =1; i<= n;i++){
-            //每个头节点的可能性 = 左边数字排布组合数 * 右边数字排布组合数
-            int left = getBstNum(i-1);
-            int right = getBstNum(n-i);
-            //累加上左右组合数的乘积
-            res += left * right;
+        int[] dp = new int[n+1];
+        //空bst和单个节点bst都只有一种排布方式
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i<=n; i++){
+            //注意从一开始可以作为头节点，到最后n可以作为头节点，全部的可能性加起来，所以是(=1, <=n)
+            for (int j = 1; j<= i; j++){
+                //i = 2, j = 2,j<=2
+                //dp[2] += dp[0] * dp[2-1] = 1*1
+                //dp[2] += dp[1] * dp[2-2] = 1+1 = 2
+                dp[i] += dp[j-1] * dp[i-j];
+            }
         }
-        memo.put(n, res);
-        return res;
+        return dp[n];
     }
-
 }
