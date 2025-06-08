838f28d71cfcd421b8f1e951a9b5edf16f096b4d
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-08 18:16:56 +0100

    Time: 19 ms (94.05%) | Memory: 42.3 MB (79.26%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,25 +1,33 @@
 class Solution {
-    Map<String, Boolean> memo = new HashMap<>();
     public boolean canPartition(int[] nums) {
-        int total = 0;
-        for(int num: nums){
-            total += num;
+        // == target value = Sum(nums)/2
+        // when weight limit = value, can the value in the bag == value?
+        int tot = 0;
+        for (int num:nums){
+            tot += num;
         }
-        if (total % 2 == 1) return false;
-        int target = total/2;
-        return backtrack(nums, target, 0, 0);
-    }
+        if (tot%2 != 0) return false;
+        int target = tot/2;
+        //item number = nums.length
+        //weight limit = target
+        int[] dp = new int[target+1];
 
-    private boolean backtrack(int[] nums, int target, int index, int currentSum){
-        if (currentSum == target) return true;
-        if (currentSum > target || index >= nums.length) return false;
-
-        String key = index + "," + currentSum;
-        if (memo.containsKey(key)) return memo.get(key);
-        //选与不选只要有一个对，就return 对
-        boolean res = (backtrack(nums,target,index+1,currentSum + nums[index]) || backtrack(nums,target, index+1, currentSum));
-        memo.put(key, res);
-        
-        return res;
+        for (int j = 0; j<= target;j++){
+            if (j>= nums[0]){
+                dp[j] = nums[0];
+            }
+        }
+        //第一行刚才已经初始化过了
+        for (int i = 1; i<nums.length;i++){
+            for (int j = target; j>=0;j--){
+                //装不下, 继承之前的状态
+                if (j >= nums[i]){
+                    //装得下，才改变
+                    dp[j] = Math.max(dp[j], dp[j-nums[i]] + nums[i]);
+                }
+                if (dp[j]==target) return true;
+            }
+        }
+        return false;
     }
 }
