4a684f9733eba428f99c6db5f7892960d38b3d96
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-09 20:26:16 +0100

    Time: 23 ms (2.31%) | Memory: 58.3 MB (97.32%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxPrice = 0;
-        int lowest = Integer.MAX_VALUE;
-        for (int price: prices){
-            lowest = Math.min(lowest, price);
-            maxPrice = Math.max(maxPrice, price - lowest);
+        //dp[i][0] = money held if holding on the ith day
+        //dp[i][1] = money held if not holding stalk on the ith day
+        int[][] dp= new int[prices.length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i<prices.length;i++){
+            //持有，要么是保持之前买过的状态，要么是第一次买进
+            //因为只能买一次，所以不是Math.max(dp[i-1][0], dp[i-1][0]-prices[i]);
+            dp[i][0] = Math.max(dp[i-1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0] + prices[i]);
         }
-        return maxPrice;
+        return dp[prices.length-1][1];
     }
 }
