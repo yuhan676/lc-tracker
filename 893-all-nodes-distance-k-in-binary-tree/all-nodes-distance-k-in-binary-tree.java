a233f95bb4c61f279483f1cae4079b58cb4f1a0b
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-10 14:04:16 +0100

    Time: 15 ms (18.32%) | Memory: 42.6 MB (75.22%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,51 +8,55 @@
  * }
  */
 class Solution {
-    Map<TreeNode, Integer> distanceM = new HashMap<>();
-    List<Integer> res = new LinkedList<>();
+    Map<TreeNode, TreeNode> parent = new HashMap<>();
 
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        findDistance(root, target);
-        traverse(root, k, distanceM.getOrDefault(root, -1));
+        markParents(root);
+        Map<TreeNode, Boolean> visited = new HashMap<>();
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(target);
+        visited.put(target, true);
+        while(k>0 && !que.isEmpty()){
+            int sz = que.size();
+            for (int i = 0; i<sz;i++){
+                TreeNode top = que.poll();
+                if (top.left != null && !visited.containsKey(top.left)){
+                    que.offer(top.left);
+                    visited.put(top.left, true);
+                }
+                if (top.right != null && !visited.containsKey(top.right)){
+                    que.offer(top.right);
+                    visited.put(top.right, true);
+                }
+                if (parent.containsKey(top) && !visited.containsKey(parent.get(top))){
+                    que.offer(parent.get(top));
+                    visited.put(parent.get(top), true);
+                }
+            }
+            k--;
+        }
+        List<Integer> res = new LinkedList<>();
+        while(!que.isEmpty()){
+            res.add(que.poll().val);
+        }
         return res;
     }
-
-    public int findDistance(TreeNode root, TreeNode target){
-        //did not find the target in this subtree
-        if (root == null) return -1;
-
-        if (distanceM.containsKey(root)){
-            return distanceM.get(root);
+    public void markParents(TreeNode root){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()){
+            int sz = q.size();
+            for (int i = 0; i<sz;i++){
+                TreeNode currN = q.poll();
+                if (currN.left!=null){
+                    parent.put(currN.left, currN);
+                    q.offer(currN.left);
+                }
+                if (currN.right!=null){
+                    parent.put(currN.right, currN);
+                    q.offer(currN.right);
+                }
+            }
         }
-
-        if (root.val == target.val){
-            distanceM.put(root,0);
-            return 0;
-        }
-        //the target can only be found in one of the subtrees
-        int left = findDistance(root.left,target);
-        int right = findDistance(root.right,target);
-        if (left >=0){
-            distanceM.put(root,left +1);
-            return left +1;
-        }
-        if (right >= 0){
-            distanceM.put(root,right +1);
-            return right +1;
-        }
-        //didn't find it in this subtree
-        return -1;
     }
-
-    public void traverse(TreeNode root, int k, int currDistance){
-        if (root == null) return;
-
-        int distance = distanceM.getOrDefault(root, currDistance);
-        if(distance == k) res.add(root.val);
-        
-        traverse(root.left, k,distance+1);
-        traverse(root.right,k,distance+1);
-    }
-
-
 }
