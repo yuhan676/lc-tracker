72408168ee45b240565f3c970a4185e088dc8e59
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-13 20:53:57 +0100

    Time: 24 ms (49.15%) | Memory: 59.1 MB (92.04%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,24 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][5];
+        int[][] dp = new int[prices.length][4];
         // money of first time holding
-        dp[0][1] = -prices[0];
+        dp[0][0] = -prices[0];
         // money of first time not holding
-        dp[0][2] = 0;
+        dp[0][1] = 0;
         //money of second time holding
-        dp[0][3] = -prices[0];
+        dp[0][2] = -prices[0];
         //money of second time not holding
-        dp[0][4] = 0;
+        dp[0][3] = 0;
         for (int i = 1; i<prices.length;i++){
             //money of holding first time
-            dp[i][1] = Math.max(dp[i-1][0] - prices[i], dp[i-1][1]);
+            dp[i][0] = Math.max( -prices[i], dp[i-1][0]);
             //money of not holding first time
-            dp[i][2] = Math.max(dp[i-1][1] + prices[i], dp[i-1][2]);
+            dp[i][1] = Math.max(dp[i-1][0] + prices[i], dp[i-1][1]);
             //money of second time holding
-            dp[i][3] = Math.max(dp[i-1][3],dp[i-1][2] - prices[i]);
+            dp[i][2] = Math.max(dp[i-1][2],dp[i-1][1] - prices[i]);
             //money of second time not holding
-            dp[i][4] = Math.max(dp[i-1][3] + prices[i], dp[i-1][4]);
+            dp[i][3] = Math.max(dp[i-1][2] + prices[i], dp[i-1][3]);
         }
-        return dp[prices.length-1][4];
+        return dp[prices.length-1][3];
     }
 }
