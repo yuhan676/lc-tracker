69191744e061266f5d12fc8fe9d420e5dd9706b4
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-19 17:02:37 +0100

    Time: 1 ms (99.61%) | Memory: 57.3 MB (21.26%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,15 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         if (nums.length==1) return nums[0];
-        int res = nums[0];
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
+        int maxSum = nums[0];
+        int currSum = nums[0];
         for (int i = 1; i<nums.length;i++){
-            //卡丹算法，假如前面的和变负了，那不如重新从当前元素开始叠加
-            dp[i]= Math.max(nums[i], dp[i-1] + nums[i]);
-            //每走一步都更新最大和
-            if (dp[i]>res) res = dp[i];
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
         }
-        return res;
+        return maxSum;
+        
     }
 }
