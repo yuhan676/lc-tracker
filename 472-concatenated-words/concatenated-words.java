03e0b540aa25193e875d275107dc6553d6cdbc77
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-06-20 20:47:57 +0100

    Time: 36 ms (95.57%) | Memory: 60.4 MB (6.50%) - LeetSync

--- 472-concatenated-words/concatenated-words.java
+++ 472-concatenated-words/concatenated-words.java
@@ -1,35 +1,68 @@
 class Solution {
-    Map<String, Boolean> memo = new HashMap<>();
-
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        //这里学习一下，把string[]加入set的快捷方式:Arrays.asList()
-        Set<String> set= new HashSet<>(Arrays.asList(words));
-
         List<String> res = new LinkedList<>();
+        //一个优化：从短的word开始放进trie，比较合理,所以排序从短到长
+        Arrays.sort(words,(a,b) -> a.length() - b.length());
+        Trie trie = new Trie();
         for (String word:words){
-            //避免suffix==word本身
-            set.remove(word);
-            if (isConcatenated(word, set)){
+            if (trie.canBeConcat(word, 0,0)){
                 res.add(word);
+            }else{
+                //只有非concat可以放进trie，可以节省一些空间和时间开销
+                trie.addWord(word);
             }
-            //把word加回来
-            set.add(word);
         }
         return res;
     }
+}
 
-    private boolean isConcatenated(String s, Set<String> set){
-        if (memo.containsKey(s)) return memo.get(s);
-        //这里注意如果写成(int i =1; i<=s.length()-2;i++)就会遇到s.length()==2无法拆分的edgecase
-        for (int i =1; i<=s.length();i++){
-            String prefix = s.substring(0,i);
-            String suffix = s.substring(i);
-            if (set.contains(prefix) && (set.contains(suffix) || isConcatenated(suffix,set))){
-                memo.put(s,true);
-                return true;
+class TrieNode{
+    TrieNode[] children;
+    boolean isEnd;
+
+    TrieNode(){
+        this.children = new TrieNode[26];
+        this.isEnd = false;
+    }
+}
+
+class Trie{
+    TrieNode root;
+    Trie(){
+        this.root = new TrieNode();
+    }
+    public void addWord(String s){
+        TrieNode curr = root;
+        for (char c: s.toCharArray()){
+            if (curr.children[c-'a'] == null) curr.children[c-'a'] = new TrieNode();
+            curr = curr.children[c-'a'];
+        }
+        curr.isEnd = true;
+    }
+    //检查一个string可不可以用trie中的word组成
+    //从startindex开始检查，并记录至今为止找到的合法word的数量foundWordCount
+    public boolean canBeConcat(String s, int startIndex, int foundWordCount){
+        TrieNode curr = root;
+        for (int i = startIndex; i<s.length();i++){
+            int charIndex = s.charAt(i) - 'a';
+            //找不到下一个字母，说明这个s并不是concat word
+            if (curr.children[charIndex]==null) return false;
+            //找到了下一个字母，往下走一格
+            curr = curr.children[charIndex];
+            //假如当前这个位置是一个词的结尾
+            if (curr.isEnd){
+                //假如刚好遍历到了s的末尾了，就得检查我们至今为止发现了多少个valid word
+                if (i == s.length()-1){
+                    //假如之前已经找到1个，现在又遇到一个validword，说明至少是两个word平凑成s
+                    //假如之前找到0个，说明这个词只找到一个valid word，不合法，不是concat word
+                    return foundWordCount>=1;
+                }
+                //检查这个validword后面，从index i+1开始的地方剩余的子串能不能be concat
+                //假如可以，说明前后半段都是合法word，就说明s是concat
+                if (canBeConcat(s,i+1, foundWordCount+1)) return true;
             }
         }
-        memo.put(s,false);
+        //全部遍历完s中的字母，没能半途中return true，只好返回false
         return false;
     }
 }
