c1ce0a75ad14422e588ef857fa71c2d0cd650e03
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-12-03 15:05:09 +0000

    Time: 6 ms (4.93%) | Memory: 47.3 MB (8.45%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -28,10 +28,11 @@ class Solution {
         TreeNode root = new TreeNode(rootVal);
         int[] leftIn = Arrays.copyOfRange(inorder, 0, i);
         int[] rightIn = Arrays.copyOfRange(inorder, i+1, inorder.length);
-
+        //切post order的关键是通过inorder我们可以得到right size
+        //但是
         int rightSize = inorder.length - 1 - i;
         int[] leftPost = Arrays.copyOfRange(postorder, 0, i);
-        int[] rightPost = Arrays.copyOfRange(postorder, i, i + rightSize);
+        int[] rightPost = Arrays.copyOfRange(postorder, i, postorder.length - 1);
         root.left = buildTree(leftIn, leftPost);
         root.right = buildTree(rightIn, rightPost);
         return root;
