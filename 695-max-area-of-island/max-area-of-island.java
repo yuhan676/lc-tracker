a51bf6b9100d4c8774e1a27f3afb403160ed7422
yuhan676 <157690180+yuhan676@users.noreply.github.com>
2025-12-07 22:46:12 +0000

    Time: 3 ms (32.73%) | Memory: 46.6 MB (18.87%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,31 +1,36 @@
 class Solution {
-    int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
     int res = 0;
     int area = 0;
     public int maxAreaOfIsland(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        for (int i = 0; i<n;i++){
-            for (int j=0;j<m;j++){
+        for (int i =0; i<grid.length;i++){
+            for (int j=0; j<grid[0].length;j++){
                 if (grid[i][j]==1){
                     area = 0;
-                    dfs(grid, i, j);
+                    bfs(grid,i,j);
                 }
             }
         }
         return res;
     }
-    private void dfs(int[][] grid, int x, int y){
+    private void bfs(int[][] grid, int x, int y){
+        Queue<int[]> que = new LinkedList<>();
+        que.add(new int[] {x,y});
         grid[x][y] = 0;
         area ++;
-        for (int i =0; i<4;i++){
-            int nextX = x + dir[i][0];
-            int nextY = y + dir[i][1];
-            if (nextX<0|| nextY <0|| nextX >= grid.length || nextY>= grid[0].length){
-                continue;
-            }
-            if (grid[nextX][nextY]==1){
-                dfs(grid,nextX,nextY);
+        while(!que.isEmpty()){
+            int[] currCor = que.poll();
+            int currX = currCor[0];
+            int currY = currCor[1];
+            for (int i =0; i<4;i++){
+                int nextX = currX + dir[i][0];
+                int nextY = currY + dir[i][1];
+                if (nextX <0 || nextY <0 || nextX >= grid.length || nextY >= grid[0].length) continue;
+                if (grid[nextX][nextY]==1){
+                    grid[nextX][nextY] = 0;
+                    area ++;
+                    que.offer(new int[] {nextX,nextY});
+                }
             }
         }
         res = Math.max(res,area);
